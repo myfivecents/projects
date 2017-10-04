1c8786100144b978908281f9167bb6b352445a2c
Jay <brumfieldj@gmail.com>
2017-10-04 13:42:43 -0400

    Changes

--- testProject/src/main/java/com/techelevator/testProject/hello.java
+++ testProject/src/main/java/com/techelevator/testProject/hello.java
@@ -3,9 +3,20 @@ package com.techelevator.testProject;
 public class hello {
 	
 	public static void main(String [] args) {
-			for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Hello, Jay");
+			if (i == 0) {
+				System.out.println("Hey");
+			} else if (i == 1) {
+				System.out.println("I said, hey");
+			} else if (i == 2) {
+				System.out.println("Um, right");
+			} else if (i == 3) {
+				System.out.println("I said, HEY!");
+			} else if (i == 4) {
+				System.out.println("Next time I'm editing your for loop");
 			}
+		}
 	}
 	
 	
